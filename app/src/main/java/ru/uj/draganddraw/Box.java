86bcbb280d69afaefa9118a008df30abb4fad7e4
UJ\Блохин Евгений <evgenyblokhin@mail.ru>
2018-09-20 19:26:23 +0700

    20.0918

---
+++ app/src/main/java/ru/uj/draganddraw/Box.java
@@ -0,0 +1,28 @@
+package ru.uj.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by Blokhin Evgeny on 20.09.2018.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
